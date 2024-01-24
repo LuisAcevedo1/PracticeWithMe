e185c133ddd871c99bdb1d4f7b64507eb0dd8970
luinux <luisacevedo2003@outlook.com>
2024-01-24 17:20:43 -0600

    Terminacion del modulo output en js

--- javascript/Output.js
+++ javascript/Output.js
@@ -10,3 +10,17 @@ function Saludo(){
 function usarDocumentWrite() {
     document.write("<p>¡Hola, este texto fue escrito con document.write()!</p>");
 }
+
+
+function alerta() {
+    window.alert("SUMA DE DOS NUMEROS");
+    input1 = Number(window.prompt("Introduce el primer numero:"));
+    input2 = Number(window.prompt("Introduce el segundo numero:"));
+    window.alert("El resultado de la suma entre " + input1 + " y "+ input2 + " es: " + (input1+input2));
+}
+
+function consolas() {
+    let info = document.getElementById("consola").value;
+    document.getElementById("consolaVacia").innerText = "Mensaje envido, para verlo abre la consola";
+    console.log(info);   
+}
